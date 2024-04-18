5f376e1451663a5eecc0703ae76e2720329c4192
Jackson Donaldson <jackson88044@gmail.com>
2024-04-18 18:51:27 -0400

    begin work on wave turret

---
+++ TDGame/Assets/scripts/shootWave.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootWave : MonoBehaviour
+{
+    public float bulletSpeed = 2;
+    public float rechargeTime = 1;
+    private float nextShootTime;
+
+    public GameObject bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextShootTime = Time.time + rechargeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextShootTime){
+            GameObject b = Instantiate(bullet);
+            b.transform.position = transform.position;
+            nextShootTime = Time.time + rechargeTime;
+        }
+    }
+}
---
+++ TDGame/Assets/scripts/waveBullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waveBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public float range = 999;
+    public float bulletDamage = 5f;
+
+    void Start()
+    {
+        
+    }
+    void Update(){
+        transform.localScale += new Vector3(speed*Time.deltaTime, speed*Time.deltaTime, 0);
+        print(transform.localScale);
+
+        if(transform.localScale.x > range){
+            print(transform.localScale.x);
+            print(range);
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col){
+        enemy enemy_script = col.gameObject.GetComponent("enemy") as enemy;
+        if(enemy_script != null){
+            col.gameObject.SendMessage("ApplyDamage", bulletDamage);
+        }
+    }
+
+}
