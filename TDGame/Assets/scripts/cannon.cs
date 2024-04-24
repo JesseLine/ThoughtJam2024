e8e980b12b0aeb55eff286fef77db1077da7e191
Jackson Donaldson <jackson88044@gmail.com>
2024-04-23 21:48:33 -0400

    continue cannon work, with art

--- TDGame/Assets/scripts/cannon.cs
+++ TDGame/Assets/scripts/cannon.cs
@@ -12,6 +12,7 @@ public class cannon : MonoBehaviour
 
     public float rotationSpeed = 1;
     public GameObject bullet;
+    public GameObject turret;
 
     // Start is called before the first frame update
     void Start()
@@ -42,22 +43,22 @@ public class cannon : MonoBehaviour
         }
 
         
-        float angle = Mathf.Atan2(nearest.transform.position.y - transform.position.y, nearest.transform.position.x -transform.position.x ) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(nearest.transform.position.y - turret.transform.position.y, nearest.transform.position.x - turret.transform.position.x ) * Mathf.Rad2Deg;
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        turret.transform.rotation = Quaternion.RotateTowards(turret.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
 
         if(Time.time > nextShootTime){
             
             GameObject projectile = Instantiate(bullet);
-            projectile.transform.position = transform.position;
+            projectile.transform.position = turret.transform.position;
 
                 //Quaternion rotation = Quaternion.LookRotation (nearest.transform.position - projectile.transform.position, transform.TransformDirection(Vector3.up));
-            projectile.transform.rotation = transform.rotation;
+            projectile.transform.rotation = turret.transform.rotation;
 
             Rigidbody2D projrb = projectile.GetComponent<Rigidbody2D>() as Rigidbody2D;
 
-            Vector2 speed = transform.right;
+            Vector2 speed = turret.transform.right;
             speed.Normalize();
             speed *= bulletSpeed;
             projrb.velocity = speed;
