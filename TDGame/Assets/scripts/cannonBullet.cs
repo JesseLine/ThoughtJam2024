1db96a970fd0e17f2d446db3d940b1fa85f6df3f
Jackson Donaldson <jackson88044@gmail.com>
2024-04-24 22:47:14 -0400

    Finish cannon with card

---
+++ TDGame/Assets/scripts/cannonBullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonBullet : MonoBehaviour
+{
+    public float bulletDamage = 5.0f;
+
+
+    void OnTriggerEnter2D(Collider2D col){
+        enemy enemy_script = col.gameObject.GetComponent("enemy") as enemy;
+        if(enemy_script != null){
+            col.gameObject.SendMessage("ApplyDamage", bulletDamage);
+        }
+    }
+    
+}
