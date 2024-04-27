093b170bf28446c43fc5e6bba5b17f8f0257f83c
Jackson Donaldson <jackson88044@gmail.com>
2024-04-26 21:38:10 -0400

    Finish turbine script and card

---
+++ TDGame/Assets/scripts/turbine.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turbine : MonoBehaviour
+{
+
+    public float additionalEnergy = 1;
+    private energy energyController;
+    private bool energyIncreased = false;
+    void Start(){
+        energyController = GameObject.Find("energy").GetComponent<energy>() as energy;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if((gameObject.GetComponent<placementController>() as placementController).inPlacementMode){
+            return;
+        }
+        if(!energyIncreased){
+            energyController.energyGain += additionalEnergy;
+            energyIncreased = true;
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        if(energyIncreased){
+            energyController.energyGain -= additionalEnergy;
+        }
+    }
+}
