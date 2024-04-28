572fe0b8f16d139b04a14bbb8e84eab6fb1fa223
Jackson Donaldson <jackson88044@gmail.com>
2024-04-27 20:57:50 -0400

    Start effect management engine

---
+++ TDGame/Assets/scripts/effectListener.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static effectManager;
+
+public class effectListener : MonoBehaviour
+{
+    //see effectManager for usage
+
+    public float damageMultiplier = 1;
+    public float speedMultiplier = 1;
+
+    private Dictionary<GameObject, (effectType, float)> activeEffects;
+
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print(other);
+        effectManager manager = other.gameObject.GetComponent<effectManager>() as effectManager;
+        if(manager != null){
+            effectType type = manager.getEffectType();
+            float value = manager.getValue();
+            activeEffects[other.gameObject] = (type, value);
+
+            switch(type){
+                case effectType.towerDamageBuff:
+                    damageMultiplier *= value;
+                    break;
+                case effectType.towerSpeedBuff:
+                    speedMultiplier *= value;
+                    break;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        print(other);
+        if(activeEffects.ContainsKey(other.gameObject)){
+            effectType type = activeEffects[other.gameObject].Item1;
+            float value = activeEffects[other.gameObject].Item2;
+            activeEffects.Remove(other.gameObject);
+            switch(type){
+                case effectType.towerDamageBuff:
+                    damageMultiplier /= value;
+                    break;
+                case effectType.towerSpeedBuff:
+                    speedMultiplier /= value;
+                    break;
+            }
+        }
+    }
+}
---
+++ TDGame/Assets/scripts/effectManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effectManager : MonoBehaviour
+{
+    public float damageMultiplier;
+    public float speedMultiplier;
+
+    public enum effectType{
+        towerDamageBuff,
+        towerSpeedBuff
+    }
+
+    public effectType effect;
+    public float value;
+    //how this script works:
+    //Any aoe buff/debuf should come with a rigidbody2d, a collider set to 'trigger' mode, and this script.
+    //set 'effect' to the type of effect, and 'value' to the corresponding multiplier. Everything else should get taken care of by the object impacted.
+    
+    //Anything wishing to be impacted by an effect should have an effectListener attached, along with a collider of some kind.
+    //effectListener will keep track of all effects currently impacting the object, and publish a value to be used by other scripts.
+    
+    public effectType getEffectType(){
+        return effect;
+    }
+    public float getValue(){
+        return value;
+    }
+
+}
