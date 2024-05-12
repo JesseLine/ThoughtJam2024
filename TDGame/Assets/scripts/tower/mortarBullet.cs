8e5e2f0cf64a3839bb2ea781a9a3e24aa8559281
Jackson Donaldson <jackson88044@gmail.com>
2024-05-11 20:31:24 -0400

    mortar projectile movement

--- TDGame/Assets/scripts/tower/mortarBullet.cs
+++ TDGame/Assets/scripts/tower/mortarBullet.cs
@@ -7,10 +7,34 @@ public class mortarBullet : MonoBehaviour
     public float damage = 5.0f;
     public float speed = 1.0f;
     public Vector3 target;
+    private Rigidbody2D rb;
+    private float maxYPosition = 10f;
+    private bool startMode;
+    private float epsilon = .1f;
     void Start(){
         print("bullet init'd");
-
+        rb = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+        startMode = true;
     }
+    void FixedUpdate(){
+        if(startMode){
+            rb.AddForce(new Vector2(0, speed * Time.deltaTime));
+
+            if(transform.position.y > maxYPosition){
+                startMode = false;
+                transform.position = new Vector3(target.x, maxYPosition, target.z);
+                rb.velocity = new Vector2(0, -rb.velocity.y);
+            }
+
+        }
+        else{
+            if(Vector3.Distance(transform.position, target) < epsilon){
+                //boom
+                Destroy(gameObject);
+            }
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D col){
         enemy enemy_script = col.gameObject.GetComponent("enemy") as enemy;
         if(enemy_script != null){
