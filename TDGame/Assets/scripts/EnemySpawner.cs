b8de3e16b6f0f124d93a6377b3b37175d4530480
JesseLine <linej8765@gmail.com>
2024-05-30 16:27:13 +0200

    added slicer enemy into wave system and disabled enemy collisions with eachother

--- TDGame/Assets/scripts/EnemySpawner.cs
+++ TDGame/Assets/scripts/EnemySpawner.cs
@@ -82,8 +82,18 @@ public class EnemySpawner : MonoBehaviour
 
     private void SpawnEnemy()
     {
-        GameObject prefabToSpawn = enemyPrefabs[0];
-        Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+        if(enemiesLeftToSpawn % 4 == 1)
+        {
+            GameObject prefabToSpawn = enemyPrefabs[1];
+            Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+        }
+        else
+        {
+            GameObject prefabToSpawn = enemyPrefabs[0];
+            Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+        }
+        
+        
     }
 
     private int EnemiesPerWave()
